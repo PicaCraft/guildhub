57b58d8bdaa90420b59c7cbb8a2a84f9229dab6a
PicaCraft <fdsfsdfsdfdsfafssdfdsfsfs@gmail.com>
2025-05-24 16:13:34 +0300

    Delete futabase-v4 directory

--- futabase-v4/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Custom404() {
-  return (
-    <div className="flex h-screen justify-center items-center bg-black text-white flex-col">
-      <h1 className="text-6xl font-bold">404</h1>
-      <p className="mt-4 text-xl">Sayfa bulunamadı.</p>
-    </div>
-  )
-}
