07088195eea20e11cee719983e767b02a2ca4cbd
PicaCraft <fdsfsdfsdfdsfafssdfdsfsfs@gmail.com>
2025-05-25 18:17:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'app')))
+app.use(express.static(path.join(__dirname, 'guildhub')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'app', 'index.html'))
+  res.sendFile(path.join(__dirname, 'guildhub', 'index.html'))
 })
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'app', '404.html'))
+  res.status(404).sendFile(path.join(__dirname, 'guildhub', '404.html'))
 })
 
 app.listen(PORT, () => {
-  console.log(`Server aktif: http://localhost:${PORT}`)
+  console.log(`Sunucu aktif: http://localhost:${PORT}`)
 })
